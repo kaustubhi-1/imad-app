c9f398f779e53b6d0b61696d64825a720303e7ae
kaustubhi-1 <32132413+kaustubhi-1@users.noreply.github.com>
2017-09-20 22:25:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,14 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-two',function(req,res)){
-    res.send('article two requested and serve from here');
-});
-
-app.get('/article-three',function(req,res)){
-    res.send('article three requested and serve from here');
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
